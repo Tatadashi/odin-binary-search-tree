ea529c2b426368bc974da272420bda834474a760
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-01-04 01:11:28 -0800

    Add tree visualizer and mergeSort
    MergeSort from recursion "project". Tree visualizer/pretty print from Odin

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 class Node {
     constructor (data) {
         this.data = data;
-        this.leftChild = null;
-        this.rightChild = null;
+        this.left = null;
+        this.right = null;
     }
 }
 
@@ -13,9 +13,53 @@ class Tree {
     }
 
     buildTree(array) {
-        return array[0];
+        const sorted = mergeSort(array);
+        return new Node(sorted[0]);
     }
 }
 
-const ta = new Tree([1, 2, 3]);
-console.log(ta);
+function mergeSort(arr) {
+  if (arr.length == 1) {
+    return arr;
+  } else {
+    const halfLength = Math.ceil(arr.length / 2);
+    const arr1 = arr.slice(0, halfLength);
+    const arr2 = arr.slice(halfLength);
+    const sortedArr1 = mergeSort(arr1);
+    const sortedArr2 = mergeSort(arr2);
+
+    const newArr = [];
+    let arr1Index = 0;
+    let arr2Index = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr1Index == arr1.length) {
+        newArr.push(sortedArr2[arr2Index++]);
+      } else if (arr2Index == arr2.length) {
+        newArr.push(sortedArr1[arr1Index++]);
+      } else if (sortedArr1[arr1Index] <= sortedArr2[arr2Index]) {
+        newArr.push(sortedArr1[arr1Index++]);
+      } else {
+        newArr.push(sortedArr2[arr2Index++]);
+      }
+    }
+    return newArr;
+  }
+}
+
+const ta = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log(ta.root);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+if (node === null) {
+    return;
+}
+if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+}
+console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+}
+};
+
+prettyPrint(ta.root);
