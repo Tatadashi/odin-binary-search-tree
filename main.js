804b3c6e07a265e70775a1fdf3cd781ae7c030ac
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-01-04 02:17:54 -0800

    Create recursiveBST() and SortedUnique the array
    Merge Sort Array then convert to Set (since set all unique values) then reconvert to array.

--- main.js
+++ main.js
@@ -14,8 +14,20 @@ class Tree {
 
     buildTree(array) {
         const sorted = mergeSort(array);
-        return new Node(sorted[0]);
+        const unique = [...new Set(sorted)];
+        return recursiveBST(unique, 0, unique.length - 1);
     }
+}
+
+function recursiveBST (arr, start, end) {
+    if (start > end) {
+        return null;
+    }
+    const mid = start + Math.floor((end - start) / 2)
+    const root = new Node(arr[mid]);
+    root.left = recursiveBST(arr, start, mid - 1);
+    root.right = recursiveBST(arr, mid + 1, end);
+    return root;
 }
 
 function mergeSort(arr) {
@@ -46,20 +58,18 @@ function mergeSort(arr) {
   }
 }
 
-const ta = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(ta.root);
-
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-if (node === null) {
+  if (node === null) {
     return;
-}
-if (node.right !== null) {
+  }
+  if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-}
-console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-if (node.left !== null) {
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-}
+  }
 };
 
+const ta = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 prettyPrint(ta.root);
