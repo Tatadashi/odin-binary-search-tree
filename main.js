903d3385dbe3e5f2ea5719292b7ec9964debb9fa
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-01-04 00:55:34 -0800

    Create Tree class
    buildTree(array) not finished yet

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ class Node {
         this.rightChild = null;
     }
 }
+
+class Tree {
+    constructor (elements) {
+        this.elements = elements;
+        this.root = this.buildTree(this.elements);
+    }
+
+    buildTree(array) {
+        return array[0];
+    }
+}
+
+const ta = new Tree([1, 2, 3]);
+console.log(ta);
